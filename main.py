d4848abc2bbeeb104b1447caabf25fc31236ee9c
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-09-29 15:48:04 +0700

    Complated(BE): Scraping Data From Website BMKG and Detik, and display them both in the main app

--- main.py
+++ main.py
@@ -8,16 +8,8 @@ if __name__ == "__main__":
     print("Main Application Data from Detik")
     result = detik.extrac_data()
     detik.view_data(result)
+
+    print("")
     print("Main Application Data from BMKG")
     result = bmkg.extrac_data()
     bmkg.view_data(result)
-# import bmkg
-#
-# """
-# Main Application For Data Scrapping Website BMKG Done
-# """
-#
-# if __name__ == "__main__":
-#     print("Main Application")
-#     result = bmkg.extrac_data()
-#     bmkg.view_data(result)
